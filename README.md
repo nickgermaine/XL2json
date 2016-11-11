26fab275f8a93e4aa9757c12f30aceb952c6674c
Nick Germaine <nick@pointybracket.net>
2016-11-11 12:54:06 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# XL 2 JSON
+
+You know how sometimes clients will send translation files in excel format?  As web developers, we all know we can't really use them as is.  So I've created a program here that will convert excel documents into valid json, that you can then use in any programming language that supports json.
+
+## Setup
+
+Just run the python file.  It requires openpyxl.  It will get you to type some input in order to correctly build your data.
+
--- xlsx2json.py
+++ xlsx2json.py
@@ -6,22 +6,16 @@ import simplejson as json
 # The final results will be here
 result = {}
 
-# Make a list of all individual sheets we want to parse
+# Make a list of all individual sheets we want to parse, column=lang associations and the file location
 file_name = input("Type xlsx file location: ")
-if not file_name:
-    file_name = "imports/translations.xlsx"
 sheet_names = input("What sheets do you want to parse? (ex. common, sheet2, another sheet): ")
-if not sheet_names:
-    sheet_names = "Common, Login, Dashboard, Reports, Distribution"
-
 column_names = input("Which two columns would you like to parse? (ex. B=en, C=de, D=cz): ")
-if not column_names:
-    column_names = "B=en, C=de"
 
+# Split the comma separated inputs into lists
 sheets = sheet_names.split(", ")
-
 cn = column_names.split(", ")
 
+# Begin creating language control variable
 languages = {}
 ck = ""
 cv = ""
@@ -35,7 +29,8 @@ for c in cn:
 
     langs.append(cv)
 
-print("languages:", languages)
+
+#Open Spreadsheet
 wb = load_workbook(filename=file_name, read_only=False)
 
 for sv in sheets:
@@ -48,6 +43,8 @@ for sv in sheets:
             col = re.findall('[A-Za-z]+', cell.coordinate)[0]
             row = re.findall('[0-9]+', cell.coordinate)[0]
 
+            # Basically here we just check if the column is in our language associations and if so, build the lang
+            # object from its value
             for k, v in languages.items():
                 if col == k:
                     values.append(cell.value)
@@ -63,10 +60,13 @@ for sv in sheets:
             else:
                 count += 1
 
+        # Compile the final results variable from all parsed and organized data
         result[values[count2]] = rs
 
+
+# Finally, save the results to a json file, and print out a success message.
 with open("lang.json", "w+") as f:
     f.write(json.dumps(result))
     f.close()
 
-print(result)
+print("Excel document has been successfully transpiled to json, and saved in the current directory as lang.json")
